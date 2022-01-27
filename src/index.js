a9591f823f6f0b27996897286faf335051bb31fc
lanabel <lanabelova73@gmail.com>
2022-01-27 12:40:52 +0200

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+
+  let nar= n.toString().split('')
+  let arn= nar.reverse()
+  const revN= parseFloat(arn.join(''))
+  console.log(revN +'revN')
+  return revN 
+
 }
